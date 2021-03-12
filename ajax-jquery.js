4b8cbbff26160ac35d2bbeda19aebc8ffa859cd0
carl6227 <19104922@usc.edu.ph>
2021-03-12 11:44:38 +0800

    filter added

--- ajax-jquery.js
+++ ajax-jquery.js
@@ -5,23 +5,31 @@ $(document).ready(function () {
       dataType: "json",
       success: function (data) {
         var index = Math.floor(Math.random() * data.length);
-        myTable=$("#myTable").append('<tr><td>'+data[index].id+'</td><td>'+ data[index].name+'</td><td>'+ data[index].email+'</td><td>'+ data[index].body+'</td></tr>');
+        myTable=$("#myTable").append('<tr id="tableBody"><td>'+data[index].id+'</td><td>'+ data[index].name+'</td><td>'+ data[index].email+'</td><td>'+ data[index].body+'</td><td>'+'<button id="del">'+'delete'+'</button>'+'</td></tr>');
+     
+        $("#myInput").on("keyup", function() {
+          var value = $(this).val().toLowerCase();
+          $("#myTable #tableBody").filter(function() {
+            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+          });
+        });
       
+        //  myTable.this(data.id).hide();
         // $("#dataholder").text("id: " + data[index].id);
         // $("#dataholder2").text("name: " + data[index].name);
         // $("#dataholder3").text("email: " + data[index].email);
         // $("#dataholder4").text("body: " + data[index].body);
       },
     });
-//     setInterval(function () {
-//       $.ajax({
-//         url: "https://jsonplaceholder.typicode.com/posts",
-//         dataType: "json",
-//         success: function (data) {
-//           var index = Math.floor(Math.random() * data.length);
-//           $("#dataholder5").text("id: " + data[index].title);
-//         },
-//       });
-//     },1000);
- });
+    // setInterval(function () {
+    //   $.ajax({
+    //     url: "https://jsonplaceholder.typicode.com/posts",
+    //     dataType: "json",
+    //     success: function (data) {
+    //       var index = Math.floor(Math.random() * data.length);
+    //       $("#dataholder5").text("comments: " + data[index].body);
+    //     },
+    //   });
+    // },3000);
+});
 });
